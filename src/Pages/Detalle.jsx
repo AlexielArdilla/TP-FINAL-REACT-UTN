f649680fa7c773f50dfa814a2a2c1b4943968e57
Alejandro G. Vera <eldiosvolador@hotmail.com>
2023-05-10 00:43:59 -0300

    Centré los forms

--- src/Pages/Detalle.jsx
+++ src/Pages/Detalle.jsx
@@ -1,43 +1,43 @@
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import { getById } from "../Services/productosService";
 import './detalle.css';
 
-function Detalle(){
-  const {id} = useParams()
-  const [loading,setLoading] = useState(true)
-  const [producto,setProducto] = useState({})
+function Detalle() {
+  const { id } = useParams()
+  const [loading, setLoading] = useState(true)
+  const [producto, setProducto] = useState({})
   const [comprar, setComprar] = useState('')
   console.log("Detalle:", id)
-  
+
   useEffect(
-    ()=>{
-      const request = async ()=>{
-        try{
+    () => {
+      const request = async () => {
+        try {
           const response = await getById(id)
           // const response = await res.json()
           console.log("Producto:", response)
           setProducto(response.data)
           setLoading(false)
-        }catch(e){
+        } catch (e) {
           console.log(e)
         }
-        
+
       }
 
       request()
-      
-      
+
+
     },
     [id]
   )
-  
 
-  const handleClick = ()=>{
+
+  const handleClick = () => {
     setComprar('Gracias por su compra!!!');
   }
-  if(loading){
+  if (loading) {
     return (
       <div>
         Cargando ...
@@ -46,12 +46,12 @@ function Detalle(){
   }
   return (
     <div className="producto-lista">
-      <img src={producto.thumbnail} alt="imagen del  producto"/>
+      <img src={producto.thumbnail} alt="imagen del  producto" />
       <h1>{producto.title}</h1>
       <p id="precio">${producto.price}</p>
       <h2>{comprar}</h2>
       <br />
-      <Button variant="primary" onClick={handleClick}>Comprar</Button>
+      <Button variant="primary" size="lg" onClick={handleClick}>Comprar</Button>
     </div>
   );
 }
